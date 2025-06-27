ac857978a42802b825cc0075be3332d36c519e99
Akhil Chandewar <akhil.chandewar00@gmail.com>
2025-06-27 11:24:50 +0530

    completed go basics

---
+++ go basic/23_init/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func init(){
+	fmt.Println("initinlizing package")
+}
+
+func init(){
+	fmt.Println("initinlizing package 2")
+}
+
+func init(){
+	fmt.Println("initinlizing package 3")
+}
+
+func main() {
+	fmt.Println("inistializing main function")
+}
